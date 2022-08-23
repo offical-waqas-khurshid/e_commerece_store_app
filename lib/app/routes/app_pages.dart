0387642471b08ff2dd497f70a9a41f841e17a391
git <git>
2022-08-23 11:58:51 +0500

    changes

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+import '../modules/home_page/bindings/home_page_binding.dart';
+import '../modules/home_page/views/home_page_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME_PAGE;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME_PAGE,
+      page: () => const HomePageView(),
+      binding: HomePageBinding(),
+    ),
+  ];
+}
---
+++ lib/app/utils/appColors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primary = Color(0xff37353b);
+  static const secondary = Color(0xff6a3ee5);
+  static const primaryIconColor = Color(0xfff89812);
+  static const primaryLabelColor = Color(0xffd1d1d1);
+
+  static const kFontColorPallets = [
+    Color.fromRGBO(26, 31, 56, 1),
+    Color.fromRGBO(72, 76, 99, 1),
+    Color.fromRGBO(149, 149, 163, 1),
+  ];
+}
