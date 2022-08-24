59e8fe770359004c75f8ed8daa43367690a90ad2
git <git>
2022-08-24 18:17:57 +0500

    main pages changes

--- lib/app/modules/detail_page/views/detail_page_view.dart
+++ lib/app/modules/detail_page/views/detail_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerece_store_app/app/utils/appColors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,17 +7,31 @@ import '../controllers/detail_page_controller.dart';
 
 class DetailPageView extends GetView<DetailPageController> {
   const DetailPageView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DetailPageView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'DetailPageView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 8),
+                    width: 300,
+                    height: 330,
+                    child: Image.asset('assets/images/brown_bg_lg.png'),
+                  ),
+                  const Positioned(
+                      top: 30.5, left: 20.0, child: Icon(Icons.arrow_back)),
+                  const Positioned(
+                    right: 20.0 ,
+                      top: 30.5,  child: Icon(Icons.heart_broken_outlined)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
