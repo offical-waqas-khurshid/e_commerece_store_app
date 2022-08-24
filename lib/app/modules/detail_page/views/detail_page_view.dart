f3d520f2e91f7abb16ba035e44306f5d4b063000
git <git>
2022-08-24 19:00:59 +0500

    main pages changes

--- lib/app/modules/detail_page/views/detail_page_view.dart
+++ lib/app/modules/detail_page/views/detail_page_view.dart
@@ -2,6 +2,7 @@ import 'package:e_commerece_store_app/app/utils/appColors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/detail_page_controller.dart';
 
@@ -26,9 +27,44 @@ class DetailPageView extends GetView<DetailPageController> {
                   const Positioned(
                       top: 30.5, left: 20.0, child: Icon(Icons.arrow_back)),
                   const Positioned(
-                    right: 20.0 ,
-                      top: 30.5,  child: Icon(Icons.heart_broken_outlined)),
+                      right: 20.0,
+                      top: 30.5,
+                      child: Icon(Icons.heart_broken_outlined)),
                 ],
+              ),
+              Container(
+                height: 423,
+                width: 375,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Mulberry Clutch',
+                          style: GoogleFonts.raleway(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20),
+                        ),
+                       const SizedBox(height: 5,),
+                        Text('( With solo loop )',style: GoogleFonts.raleway(
+                            color: AppColors.primaryLabelColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text('\$799',style: GoogleFonts.poppins(
+                            color: AppColors.secondary,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24),),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
